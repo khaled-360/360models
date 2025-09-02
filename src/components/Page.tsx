42658b10b5323797a61a64e53b35b761e0f3512c
K-wadi <143789685+K-wadi@users.noreply.github.com>
2025-09-02 14:12:11 +0200

    Updated App.css

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,15 +1,12 @@
-@@ .. @@
- import { PropsWithChildren } from "react";
- import { useOverlayRenderer } from "@contexts/useOverlayRenderer.tsx";
+import { PropsWithChildren } from "react";
+import { useOverlayRenderer } from "@contexts/useOverlayRenderer.tsx";
 
- export function Page({ children }: PropsWithChildren) {
-     const { renderer } = useOverlayRenderer();
-     return (
--        <>
-+        <div className="flex h-full flex-col overflow-hidden">
-             {children}
-             {renderer}
--        </>
-+        </div>
-     );
- }
+export function Page({ children }: PropsWithChildren) {
+    const { renderer } = useOverlayRenderer();
+    return (
+        <div className="flex h-full flex-col overflow-hidden animate-fade-in">
+            {children}
+            {renderer}
+        </div>
+    );
+}
