71c3d3d1a899f3f306df19599e00bb239dd043d7
khaled-360 <khaled@360fabriek.nl>
2025-09-02 13:37:50 +0200

    Updated PanelLayout.tsx

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,8 +3,11 @@
  import { LoadingSpinner } from "@components/LoadingSpinner/LoadingSpinner.tsx";
 
 -export function Loading({ text }) {
+}
 +export function Loading({ text }: { text: string }) {
      return (
+     )
+}
 -        <div className="fixed bottom-0 left-0 right-0 top-0 z-10 flex flex-col items-center justify-center gap-4 bg-[#000000aa] text-white">
 -            <LoadingSpinner />
 -            <p>{text}</p>
